6cc2961ee887d05b8f587aff27facf990403a43e
Ayna <aynazewd@yahoo.com>
2019-02-21 00:44:11 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.aadea2d54141f3a5f0b6d4ea0781aa84.js"
+  "/portfolio/precache-manifest.24d2cdaa06d9365ceb397f0b8c0032ba.js"
 );
 
 workbox.clientsClaim();
